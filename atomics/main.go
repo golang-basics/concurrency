ab75a77ed10550258514f71a042c72b416a0ee91
steevehook <steevehook@gmail.com>
2021-04-22 06:58:30 +0300

    add atomics example

--- atomics/main.go
+++ atomics/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
 func main() {
+	var count int64 = 0
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		time.Sleep(10*time.Millisecond)
+		fmt.Println(atomic.LoadInt64(&count))
+		wg.Done()
+	}()
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			time.Sleep(10*time.Millisecond)
+			atomic.AddInt64(&count, 1)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(count)
 }
