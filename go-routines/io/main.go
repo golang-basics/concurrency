48938f8378946cce0910f1ca07ef5860e81b6e20
steevehook <steevehook@gmail.com>
2021-04-23 10:44:35 +0300

    add io and netpoller examples

--- go-routines/io/main.go
+++ go-routines/io/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "runtime"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
 	runtime.GOMAXPROCS(1)
+	go fmt.Println("I will print in a different thread")
+	// the call below is a syscall, meaning it is a synchronous call
+	// a separate thread will take care of it to avoid blocking the existing thread
+	_, _ = os.Create("test.txt")
 }
--- go-routines/netpoller/main.go
+++ go-routines/netpoller/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "runtime"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+)
 
 func main() {
 	runtime.GOMAXPROCS(1)
+	go fmt.Println("I am running in different thread")
+	// runs in net poller thread
+	_, _ = http.Get("https://www.google.com")
 }
