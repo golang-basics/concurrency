165afc3e9deaea094e4b9ffe6538120f9afafba2
steevehook <steevehook@gmail.com>
2021-04-23 11:56:37 +0300

    add trace example

---
+++ go-routines/trace/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/trace"
+	"sync"
+)
+
+// view trace after running the program by running the command
+// go tool trace trace.out
+func main() {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		go func() {
+			t := 0
+			for i:=0;i<100;i++ {
+				t+=2
+			}
+			fmt.Println("total:", t)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
