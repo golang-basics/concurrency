6a1d0584fcd8d8b798519ba1a6e493bedacdda8b
steevehook <steevehook@gmail.com>
2021-04-30 18:10:06 +0300

    add quit pattern

---
+++ patterns/quit/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, os.Interrupt)
+
+	err := worker(quit)
+
+	select {
+	case e := <-err:
+		fmt.Println("error happened in worker:", e)
+	default:
+		return
+	}
+}
+
+func worker(quit chan os.Signal) chan error {
+	ticker := time.NewTicker(500*time.Millisecond)
+	timer := time.NewTimer(3*time.Second)
+	for {
+		select {
+		case <-timer.C:
+			err := make(chan error, 1)
+			err <- errors.New("something wrong happened")
+			return err
+		case <-ticker.C:
+			fmt.Println("doing some work")
+		case <-quit:
+			cleanup()
+			fmt.Println("exiting")
+			return nil
+		}
+	}
+}
+
+func cleanup() {
+	fmt.Println("doing some cleanup before exiting")
+}
