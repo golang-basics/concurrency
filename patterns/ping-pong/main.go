fb2308e0f2c70c2177d2797961dff8a68b6624db
steevehook <steevehook@gmail.com>
2021-05-07 01:30:11 +0300

    add ping-pong pattern example

--- patterns/ping-pong/main.go
+++ patterns/ping-pong/main.go
@@ -1,4 +1,33 @@
+// The ping pong pattern is very simple and useful in a lot of cases.
+// It basically does a ping pong in terms of concurrency.
+// The data is ping-ponged between multiple go routines back an forth
+// There's constantly some go routine receiving data (PING) and re-passing (PONG)
+// to another go routine listening on the same data.
+
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	table := make(chan int)
+	go player(table, 1)
+	go player(table, 2)
+	go player(table, 3)
+
+	table <- 0
+	time.Sleep(1 * time.Second)
+	fmt.Println(<-table)
+}
+
+func player(table chan int, playerNo int) {
+	for {
+		ball := <-table
+		fmt.Println("got ball:", ball, "from player:", playerNo)
+		ball++
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
+	}
 }
