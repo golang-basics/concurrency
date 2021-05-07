808aa0bec3d04147d32cd87661848b7b0fa9c16f
steevehook <steevehook@gmail.com>
2021-05-07 04:06:26 +0300

    add error handling pattern example

---
+++ patterns/error-handling/main.go
@@ -0,0 +1,67 @@
+// The best practice when it comes to error handling in a concurrent world
+// is to tie them to responses, or wrap them inside a struct where both: the result and the error
+// is present, this way each go routine would only care about sending a struct
+// and let the caller decide better how to handle the error in case there is one.
+// You can find more about the better error handling in concurrent situations on:
+// https://www.oreilly.com/library/view/concurrency-in-go/9781491941294/ch04.html
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Result struct {
+	Error    error
+	Response *http.Response
+}
+
+func main() {
+	done := make(chan interface{})
+	defer close(done)
+
+	errCount := 0
+	urls := []string{
+		"https://www.google.com",
+		"https://badhost1",
+		"https://badhost2",
+		"https://badhost3",
+		"https://www.google.com",
+		"https://www.google.com",
+		"https://www.google.com",
+	}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v\n", result.Error)
+			errCount++
+			if errCount >= 3 {
+				fmt.Println("Too many errors, breaking!")
+				break
+			}
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response.Status)
+	}
+}
+
+func checkStatus(done <-chan interface{}, urls ...string) <-chan Result {
+	results := make(chan Result)
+	go func() {
+		defer close(results)
+		for _, url := range urls {
+			var result Result
+			response, err := http.Get(url)
+			result = Result{
+				Error:    err,
+				Response: response,
+			}
+			select {
+			case <-done:
+				return
+			case results <- result:
+			}
+		}
+	}()
+	return results
+}
