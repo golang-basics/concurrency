30ec33133b1c441f03316ecc0d76e65a48fdcfef
steevehook <steevehook@gmail.com>
2021-05-30 20:20:40 +0300

    add couple of dummy slides + add more go routine examples

---
+++ go-routines/leak/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	// usually 1 go routine goes for main
+	// it automatically runs in its own go routine
+	fmt.Println("go routines before work:", runtime.NumGoroutine())
+	go work(nil)
+	timer := time.NewTimer(5*time.Second)
+	for {
+		select {
+		case <-timer.C:
+			fmt.Println("exiting")
+			return
+		default:
+		}
+		time.Sleep(time.Second)
+		fmt.Println("number of go routines:", runtime.NumGoroutine())
+	}
+}
+
+func work(strings <-chan string) {
+	defer fmt.Println("work is done")
+	for s := range strings {
+		fmt.Println(s)
+	}
+}
---
+++ go-routines/no-leak-done-channel/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	fmt.Println("go routines before work:", runtime.NumGoroutine())
+	// conventionally it's called done to signal cancellation of processes
+	// also conventionally it's a chan os struct{}, we just need to signal
+	// we don't need to pass any kind of memory around go routines
+	// also a a convention the done channel is the 1st param, but it does not have to be
+	done := make(chan struct{})
+	go work(done, nil)
+	go func() {
+		time.Sleep(time.Second)
+		fmt.Println("cancelling all go routines, listening on done")
+		close(done)
+	}()
+	fmt.Println("number of go routines while working:", runtime.NumGoroutine())
+
+	time.Sleep(2*time.Second)
+	fmt.Println("number of go routines after closing done:", runtime.NumGoroutine())
+}
+
+func work(done chan struct{}, strings <-chan string) {
+	defer fmt.Println("work is done")
+	for {
+		select {
+		case s := <-strings:
+			fmt.Println(s)
+		case <-done:
+			return
+		}
+	}
+}
