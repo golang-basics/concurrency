d3f54b5e4d18dc09bf8afe45258af1fe1aedd4fb
steevehook <steevehook@gmail.com>
2021-06-01 01:34:11 +0300

    add fan-in-out example

--- patterns/fan-in-out/main.go
+++ patterns/fan-in-out/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	"concurrency/patterns/fanin"
+	"concurrency/patterns/fanout"
+	"concurrency/patterns/generator"
+)
+
+var (
+	intGen = generator.OddIntGen
+	fanOut = fanout.FanOut
+	fanIn  = fanin.FanIn
+)
+
 func main() {
+	odd := intGen(20)
+	c1, c2, c3 := fanOut(odd), fanOut(odd), fanOut(odd)
+	for v := range fanIn(c1, c2, c3) {
+		fmt.Println("value:", v)
+	}
 }
