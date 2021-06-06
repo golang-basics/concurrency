ee3122fb714aec6b44b917c02e9e687752c7c46c
steevehook <steevehook@gmail.com>
2021-06-06 07:20:04 +0300

    slightly improve the tee channel

--- patterns/tee-channel/main.go
+++ patterns/tee-channel/main.go
@@ -20,55 +20,59 @@ func main() {
 	out := tee(done, nums, 3)
 	ch1, ch2, ch3 := out[0], out[1], out[2]
 
-	for v := range pipeline.Inc(done, ch1) {
-		fmt.Println("incremented:", v)
-	}
-	for v := range pipeline.Dec(done, ch2) {
-		fmt.Println("decremented:", v)
-	}
-	for v := range pipeline.Sq(done, ch3) {
-		fmt.Println("squared:", v)
-	}
-}
-
-// maybe generate n clone channels
-// for example: ls | tee f1.txt f2.txt f3.txt
-func tee(done <-chan struct{}, in <-chan int, n int) []chan int {
 	var wg sync.WaitGroup
-	wg.Add(1)
-	values := make([]int, 0)
+	wg.Add(3)
 	go func() {
 		defer wg.Done()
+		for v := range pipeline.Inc(done, ch1) {
+			fmt.Println("incremented:", v)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for v := range pipeline.Dec(done, ch2) {
+			fmt.Println("decremented:", v)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for v := range pipeline.Sq(done, ch3) {
+			fmt.Println("squared:", v)
+		}
+	}()
+	wg.Wait()
+}
+
+// The tee channel works pretty much like the tee Unix command.
+// Try this command: ls | tee f1.txt f2.txt f3.txt
+// which will list the current directory contents to stdout and the above files
+func tee(done <-chan struct{}, in <-chan int, n int) []chan int {
+	channels := make([]chan int, 0)
+	for i := 0; i < n; i++ {
+		channels = append(channels, make(chan int))
+	}
+	go func() {
+		// close all outbound channels at the end
+		defer func() {
+			for _, in := range channels {
+				close(in)
+			}
+		}()
+
+		// range over the streamed values
 		for val := range in {
 			select {
 			case <-done:
 				return
 			default:
-				values = append(values, val)
+				for _, ch := range channels {
+					// Keep in mind, each channel must have a go routine
+					// that reads from it, any kind of blocking operation/ignorance
+					// will result in a deadlock. So make sure to use all channels correctly.
+					ch <- val
+				}
 			}
 		}
 	}()
-	wg.Wait()
-
-	channels := make([]chan int, 0)
-	defer func() {
-		for _, ch := range channels {
-			close(ch)
-		}
-	}()
-
-	for i := 0; i < n; i++ {
-		channels = append(channels, make(chan int, len(values)))
-	}
-	for _, val := range values {
-		select {
-		case <-done:
-			break
-		default:
-			for _, ch := range channels {
-				ch <- val
-			}
-		}
-	}
 	return channels
 }
