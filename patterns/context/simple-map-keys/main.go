74e5bfb8f03c1d5fa05a51a88e62f3452474de01
steevehook <steevehook@gmail.com>
2021-06-08 08:23:45 +0300

    add simple map keys example

--- patterns/context/simple-map-keys/main.go
+++ patterns/context/simple-map-keys/main.go
@@ -1,4 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	type foo int
+	type bar int
+
+	people := map[interface{}]string{
+		0:               "Nobody",
+		foo(0000):       "John",
+		0 + 0i + bar(0): "Steve",
+		int32(0):        "int32",
+		int64(0):        "int64",
+	}
+	fmt.Println(people)
+	fmt.Println(people[0000])
 }
