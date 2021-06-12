cf7343fe5f55aa8daee26c6e13c1cf596c4fca90
steevehook <steevehook@gmail.com>
2021-06-13 01:44:40 +0300

    wrap up with the errors rest api example

--- patterns/error-handling/http/repositories/bookings.go
+++ patterns/error-handling/http/repositories/bookings.go
@@ -48,17 +48,38 @@ func (r BookingsRepository) Init(numberOfRooms int) error {
 	return nil
 }
 
+// GetBooking fetches a booking from the database
+func (r BookingsRepository) GetBooking(ctx context.Context, id string) (models.Booking, error) {
+	logger := logging.Logger()
+	booking := models.Booking{}
+	err := r.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(bookingsBucket))
+		notFoundErr := models.ResourceNotFoundError{
+			Message: "could not find booking with id: " + id,
+		}
+		if bucket == nil {
+			return notFoundErr
+		}
+
+		bs := bucket.Get([]byte(id))
+		if len(bs) == 0 {
+			return notFoundErr
+		}
+		return json.Unmarshal(bs, &booking)
+	})
+	if err != nil {
+		logger.Error("could not fetch booking", zap.Error(err))
+		return models.Booking{}, err
+	}
+
+	return booking, nil
+}
+
 // CreateBooking creates and saves a booking inside the database
 func (r BookingsRepository) CreateBooking(ctx context.Context, booking models.Booking) (models.Booking, error) {
 	logger := logging.Logger()
-
-	// to avoid creating hotels operations
-	// we use this hardcoded value, for brevity and simplicity
 	if booking.HotelID != models.DefaultHotelID {
-		err := models.ResourceNotFoundError{
-			Message: "could not find hotel with id: " + booking.HotelID,
-		}
-		return models.Booking{}, err
+		return models.Booking{}, r.hotelNotFoundError(booking.HotelID)
 	}
 
 	isFull := true
@@ -81,16 +102,8 @@ func (r BookingsRepository) CreateBooking(ctx context.Context, booking models.Bo
 			logger.Error("could not create bookings bucket", zap.Error(err))
 			return err
 		}
-
-		bs, err := json.Marshal(booking)
+		err = r.save(bucket, booking.ID, booking)
 		if err != nil {
-			logger.Error("could not marshal booking", zap.Error(err))
-			return err
-		}
-
-		err = bucket.Put([]byte(booking.ID), bs)
-		if err != nil {
-			logger.Error("could not save booking in db", zap.Error(err))
 			return err
 		}
 
@@ -99,55 +112,9 @@ func (r BookingsRepository) CreateBooking(ctx context.Context, booking models.Bo
 			logger.Error("could not create rooms bucket", zap.Error(err))
 			return err
 		}
-
-		bs, err = json.Marshal(r.rooms[booking.HotelID])
-		if err != nil {
-			logger.Error("could not marshal rooms", zap.Error(err))
-			return err
-		}
-
-		err = bucket.Put([]byte(booking.HotelID), bs)
-		if err != nil {
-			logger.Error("could not save rooms in db", zap.Error(err))
-			return err
-		}
-		return nil
+		return r.save(bucket, booking.HotelID, r.rooms[booking.HotelID])
 	})
 	if err != nil {
-		return models.Booking{}, err
-	}
-
-	return booking, nil
-}
-
-// GetBooking fetches a booking from the database
-func (r BookingsRepository) GetBooking(ctx context.Context, id string) (models.Booking, error) {
-	logger := logging.Logger()
-	booking := models.Booking{}
-	err := r.db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(bookingsBucket))
-		notFoundErr := models.ResourceNotFoundError{
-			Message: "could not find booking with id: " + id,
-		}
-		if bucket == nil {
-			return notFoundErr
-		}
-
-		bs := bucket.Get([]byte(id))
-		if len(bs) == 0 {
-			return notFoundErr
-		}
-
-		err := json.Unmarshal(bs, &booking)
-		if err != nil {
-			logger.Error("could not unmarshal booking", zap.Error(err))
-			return err
-		}
-
-		return nil
-	})
-	if err != nil {
-		logger.Error("could not fetch booking", zap.Error(err))
 		return models.Booking{}, err
 	}
 
@@ -162,7 +129,6 @@ func (r BookingsRepository) DeleteExpiredBookings(ctx context.Context) (int, err
 		if bucket == nil {
 			return nil
 		}
-
 		err := bucket.ForEach(func(k, v []byte) error {
 			var booking models.Booking
 			err := json.Unmarshal(v, &booking)
@@ -188,14 +154,9 @@ func (r BookingsRepository) DeleteExpiredBookings(ctx context.Context) (int, err
 		if bucket == nil {
 			return nil
 		}
-
 		for _, booking := range bookings {
 			r.rooms[booking.HotelID][booking.RoomNumber] = false
-			bs, err := json.Marshal(r.rooms[booking.HotelID])
-			if err != nil {
-				return err
-			}
-			err = bucket.Put([]byte(booking.HotelID), bs)
+			err := r.save(bucket, booking.HotelID, r.rooms[booking.HotelID])
 			if err != nil {
 				return err
 			}
@@ -203,7 +164,7 @@ func (r BookingsRepository) DeleteExpiredBookings(ctx context.Context) (int, err
 		return nil
 	})
 	if err != nil {
-		logger.Error("could not fetch rooms", zap.Error(err))
+		logger.Error("could not delete expired bookings", zap.Error(err))
 		return 0, err
 	}
 	return len(bookings), nil
@@ -223,13 +184,10 @@ func (r BookingsRepository) getRooms(hotelID string, numberOfRooms int) (map[int
 		}
 
 		bs := bucket.Get([]byte(hotelID))
-		err := json.Unmarshal(bs, &rooms)
-		if err != nil {
-			logger.Error("could not unmarshal rooms", zap.Error(err))
-			return err
+		if len(bs) == 0 {
+			return r.hotelNotFoundError(hotelID)
 		}
-
-		return nil
+		return json.Unmarshal(bs, &rooms)
 	})
 	if err != nil {
 		logger.Error("could not fetch rooms", zap.Error(err))
@@ -237,4 +195,25 @@ func (r BookingsRepository) getRooms(hotelID string, numberOfRooms int) (map[int
 	}
 
 	return rooms, nil
+}
+
+func (r BookingsRepository) save(bucket *bolt.Bucket, key string, value interface{}) error {
+	logger := logging.Logger()
+	bs, err := json.Marshal(value)
+	if err != nil {
+		logger.Error("could not marshal data", zap.Error(err))
+		return err
+	}
+	err = bucket.Put([]byte(key), bs)
+	if err != nil {
+		logger.Error("could not put data inside bolt db", zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func (r BookingsRepository) hotelNotFoundError(hotelID string) error {
+	return models.ResourceNotFoundError{
+		Message: "could not find hotel with id: " + hotelID,
+	}
 }
