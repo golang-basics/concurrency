3bbc296843ca27deb6f1fe6321e1964ee786ab8b
steevehook <steevehook@gmail.com>
2021-06-14 01:24:40 +0300

    add replicated requests example

--- patterns/replicated-requests/main.go
+++ patterns/replicated-requests/main.go
@@ -1,4 +1,51 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 func main() {
+	done := make(chan struct{})
+	result := make(chan int)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go work(done, result, i+1, &wg)
+	}
+
+	first := <-result
+	close(done)
+	wg.Wait()
+
+	fmt.Printf("received an answer from: #%d\n", first)
+}
+
+func work(done chan struct{}, result chan<- int, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	started := time.Now()
+	latency := time.Duration(1+rand.Intn(5)) * time.Second
+	select {
+	case <-done:
+		// we normally return here,
+		// but the below time print is negligible
+	case <-time.After(latency):
+	}
+
+	select {
+	case <-done:
+	// we normally return here,
+	// but the below time print is negligible
+	case result <- id:
+	}
+
+	took := time.Since(started)
+	if took < latency {
+		took = latency
+	}
+	fmt.Printf("#%d took: %v\n", id, took)
 }
