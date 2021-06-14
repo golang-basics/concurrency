7b2276ecc0a5b4dbd2510acc22435daeeb078aef
steevehook <steevehook@gmail.com>
2021-06-14 19:44:03 +0300

    add healing unhealthy go routines example

--- patterns/healing-unhealthy-goroutines/main.go
+++ patterns/healing-unhealthy-goroutines/main.go
@@ -1,4 +1,105 @@
 package main
 
+import (
+	"log"
+	"os"
+	"time"
+)
+
 func main() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	done := make(chan struct{})
+	time.AfterFunc(11*time.Second, func() {
+		close(done)
+	})
+
+	steward := newSteward(4*time.Second, ward)
+	for range steward(done, 4*time.Second) {
+	}
+	log.Println("done")
+}
+
+// startGoroutineFn represents the function that can be monitored
+type startGoroutineFn func(done <-chan struct{}, interval time.Duration) (heartbeat <-chan struct{})
+
+// ward represents really any concurrent process that will be monitored
+// by a steward (supervisor) go routine.
+func ward(done <-chan struct{}, interval time.Duration) <-chan struct{} {
+	heartbeat := make(chan struct{})
+	log.Println("ward: hello")
+	pulse := time.Tick(interval)
+	var i int
+	go func() {
+		for {
+			if i == 2 {
+				// break things on purpose
+				// to simulate some kind of
+				// death spiral
+				log.Println("ward: death spiral")
+				pulse = nil
+			}
+			select {
+			case <-done:
+				log.Println("ward: halting")
+				return
+			case <-pulse:
+				log.Println("ward: pulse")
+				select {
+				case heartbeat <- struct{}{}:
+					i++
+				default:
+				}
+			}
+		}
+	}()
+	return heartbeat
+}
+
+// newSteward runs a go routine that monitors other go routines.
+// Notice, it also returns startGoroutineFn which itself can be monitored
+func newSteward(timeout time.Duration, startGoroutine startGoroutineFn) startGoroutineFn {
+	return func(done <-chan struct{}, interval time.Duration) <-chan struct{} {
+		heartbeat := make(chan struct{})
+		go func() {
+			defer close(heartbeat)
+
+			var wardDone chan struct{}
+			var wardHeartbeat <-chan struct{}
+			startWard := func() {
+				wardDone = make(chan struct{})
+				wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
+			}
+
+			startWard()
+			pulse := time.Tick(interval)
+
+		loop:
+			for {
+				timer := time.After(timeout)
+
+				for {
+					select {
+					case <-pulse:
+						log.Println("steward: pulse")
+						select {
+						case heartbeat <- struct{}{}:
+						default:
+						}
+					case <-wardHeartbeat:
+						continue loop
+					case <-timer:
+						log.Println("steward: ward is unhealthy; restarting")
+						close(wardDone)
+						startWard()
+						continue loop
+					case <-done:
+						return
+					}
+				}
+			}
+		}()
+		return heartbeat
+	}
 }
---
+++ patterns/healing-unhealthy-goroutines/or.go
@@ -0,0 +1,30 @@
+package main
+
+func or(channels ...<-chan struct{}) <-chan struct{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan struct{})
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
+}
