e9deadfd36934f0977c8f1bf93c46243db3bdbee
steevehook <steevehook@gmail.com>
2021-06-19 11:39:35 +0300

    add go-routines examples in the readme

--- go-routines/async-preemption/main.go
+++ go-routines/async-preemption/main.go
@@ -2,6 +2,7 @@ package main
 
 import "runtime"
 
+// Asynchronous Preemption Go >= 1.14
 func main() {
 	runtime.GOMAXPROCS(1)
 	go println("goroutine ran")
--- go-routines/io/main.go
+++ go-routines/io/main.go
@@ -11,5 +11,6 @@ func main() {
 	go fmt.Println("I will print in a different thread")
 	// the call below is a syscall, meaning it is a synchronous call
 	// a separate thread will take care of it to avoid blocking the existing thread
+	// try a long running write operation
 	_, _ = os.Create("test.txt")
 }
