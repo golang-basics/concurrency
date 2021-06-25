7152d5e23c07069ce9b0c884a884638c7ee482bf
steevehook <steevehook@gmail.com>
2021-06-25 08:38:31 +0300

    add race miss example

--- go-routines/race-condition/race-miss/main.go
+++ go-routines/race-condition/race-miss/main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"sync"
+)
+
+// Try and run this program using the -race flag
+// it will not display any race condition
+// even if the code has a race condition.
+// The race detector only works on running code.
+// ---------------------------------------------
+// The race condition will get detected only when
+// the -inc and -dec flags are provided like so:
+// go run -race -inc -dec
 func main() {
-	// simulate race condition that is not detected
-	// add another example with output option for race flag
+	count := 0
+	incrementFlag := flag.Bool("inc", false, "increment counter")
+	decrementFlag := flag.Bool("dec", false, "decrement counter")
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		if *incrementFlag {
+			count++
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		if *decrementFlag {
+			count--
+		}
+	}()
+	wg.Wait()
+	fmt.Println(count)
 }
