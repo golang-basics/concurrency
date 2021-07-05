a14985cd5773b7361e98d4595d31c289eabdc533
steevehook <steevehook@gmail.com>
2021-07-05 23:39:05 +0300

    adjust the value example in the atomics

--- atomics/value/main.go
+++ atomics/value/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
 	"sync/atomic"
 )
@@ -15,6 +16,11 @@ func main() {
 	var wg sync.WaitGroup
 
 	// writer
+	// we need to wait for the first value
+	// to be stored, otherwise the type assertion
+	// will fail and panic
+	wg.Add(1)
+	var once sync.Once
 	go func() {
 		var i int
 		for {
@@ -23,14 +29,24 @@ func main() {
 				a: []int{i + 1, i + 2, i + 3, i + 4, i + 5},
 			}
 			v.Store(cfg)
+			once.Do(func() {
+				wg.Done()
+			})
 		}
 	}()
 
 	// reader
+	// wait for the first value to be stored
+	wg.Wait()
+	wg.Add(5)
 	for i := 0; i < 5; i++ {
-		wg.Add(1)
 		go func() {
-			cfg := v.Load()
+			// we're gonna get a panic this way
+			// cfg := v.Load().(Config)
+			cfg, ok := v.Load().(Config)
+			if !ok {
+				log.Fatalf("received different type: %T", cfg)
+			}
 			fmt.Println("cfg", cfg)
 			wg.Done()
 		}()
