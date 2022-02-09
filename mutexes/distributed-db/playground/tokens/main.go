f6233a1759c6c6cb9577f37a1cec80b6fd017e94
steevehook <steevehook@gmail.com>
2022-02-09 14:19:09 +0200

    add token range example in the distributed-db playground

--- mutexes/distributed-db/playground/tokens/main.go
+++ mutexes/distributed-db/playground/tokens/main.go
@@ -10,23 +10,59 @@ import (
 )
 
 func main() {
-	tokens := NewTokens(5, 256)
-	for i := 0; i < 50; i++ {
+	nodes := []Node{
+		{
+			ID: 1,
+			Name: "server1",
+			Address: "localhost:8081",
+		},
+		{
+			ID: 2,
+			Name: "server2",
+			Address: "localhost:8082",
+		},
+		{
+			ID: 3,
+			Name: "server3",
+			Address: "localhost:8083",
+		},
+		{
+			ID: 4,
+			Name: "server4",
+			Address: "localhost:8084",
+		},
+		{
+			ID: 5,
+			Name: "server5",
+			Address: "localhost:8085",
+		},
+	}
+	tokens := NewTokens(nodes, 256)
+
+	newNode := Node{
+		ID: 6,
+		Name: "server6",
+		Address: "localhost:8086",
+	}
+	tokens.AddNode(newNode)
+
+	for i := 0; i < 1000; i++ {
 		key := fmt.Sprintf("k%d", i+1)
 		sum := hash(key)
-		fmt.Println(key, sum, tokens.Get(sum))
+		fmt.Println(key, sum, tokens.GetNode(sum).Name)
 	}
 }
 
-func NewTokens(numberOfNodes, numberOfTokenRanges int) Tokens {
-	servers := make([]string, numberOfNodes)
-	tokenRange := math.MaxInt / numberOfNodes / numberOfTokenRanges
+func NewTokens(nodes []Node, numberOfTokenRanges int) Tokens {
+	numberOfNodes := len(nodes)
+	tokenRange := uint64(math.MaxInt / numberOfNodes / numberOfTokenRanges)
 	ranges := make([]uint64, 0, numberOfNodes*numberOfTokenRanges)
+	nodesMap := map[int]Node{}
 	for i := 0; i < numberOfNodes; i++ {
-		servers[i] = fmt.Sprintf("server%d", i+1)
+		nodesMap[nodes[i].ID] = nodes[i]
 		for j := numberOfTokenRanges * i; j < numberOfTokenRanges*(i+1); j++ {
-			r := tokenRange * (j + 1)
-			ranges = append(ranges, uint64(r))
+			r := tokenRange * uint64(j + 1)
+			ranges = append(ranges, r)
 		}
 	}
 
@@ -36,9 +72,9 @@ func NewTokens(numberOfNodes, numberOfTokenRanges int) Tokens {
 		randomRanges[i], randomRanges[j] = randomRanges[j], randomRanges[i]
 	})
 
-	i, mappings := 0, map[uint64]string{}
+	i, mappings := 0, map[uint64]int{}
 	for _, r := range randomRanges {
-		mappings[r] = servers[i]
+		mappings[r] = nodes[i].ID
 		i++
 		if i == numberOfNodes {
 			i = 0
@@ -48,20 +84,55 @@ func NewTokens(numberOfNodes, numberOfTokenRanges int) Tokens {
 	tokens := Tokens{
 		Ranges:   ranges,
 		Mappings: mappings,
+		Nodes: nodesMap,
+		NumberOfTokenRanges: numberOfTokenRanges,
 	}
 	return tokens
 }
 
 type Tokens struct {
-	Ranges   []uint64
-	Mappings map[uint64]string
+	Ranges              []uint64
+	Mappings            map[uint64]int
+	Nodes               map[int]Node
+	NumberOfTokenRanges int
+	rangeMax uint64
 }
 
-func (t *Tokens) Get(n uint64) string {
+func (t *Tokens) GetNode(token uint64) Node {
 	idx := sort.Search(len(t.Ranges)-1, func(i int) bool {
-		return t.Ranges[i] >= n
+		return t.Ranges[i] >= token
 	})
-	return t.Mappings[t.Ranges[idx]]
+	nodeID := t.Mappings[t.Ranges[idx]]
+	return t.Nodes[nodeID]
+}
+
+func (t *Tokens) AddNode(node Node) {
+	t.Nodes[node.ID] = node
+	tokenRange := uint64(math.MaxInt / len(t.Nodes) / t.NumberOfTokenRanges)
+	newRanges := make([]uint64, 0, t.NumberOfTokenRanges)
+	for i := 0; i < len(t.Ranges)+t.NumberOfTokenRanges; i++ {
+		if i < len(t.Ranges) {
+			r := t.Ranges[i]
+			decrement := r - tokenRange*(uint64(i+1))
+			newRange := r - decrement
+			srv := t.Mappings[r]
+
+			delete(t.Mappings, r)
+			t.Mappings[newRange] = srv
+			t.Ranges[i] = newRange
+		} else {
+			newRange := tokenRange * uint64(i+1)
+			t.Mappings[newRange] = len(t.Nodes)
+			newRanges = append(newRanges, newRange)
+		}
+	}
+	t.Ranges = append(t.Ranges, newRanges...)
+}
+
+type Node struct {
+	ID   int
+	Name string
+	Address string
 }
 
 func hash(s string) uint64 {
